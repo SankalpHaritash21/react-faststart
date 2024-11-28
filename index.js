18baaeca2ffc1f7fd2b305588b4e0b4009d79d11
Sankalp <sanklpkaushik21@gmail.com>
2024-11-28 17:26:39 +0530

    feature: adding options between tailwind css and default

--- index.js
+++ index.js
@@ -1,21 +1,129 @@
 #!/usr/bin/env node
 
-import { execa } from "execa";
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
+import chalk from "chalk";
+import { execa } from "execa";
 import inquirer from "inquirer";
+import { fileURLToPath } from "url";
+import { checkAppName } from "./src/helper/reactApp.js";
+
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split(".");
+const major = semver[0];
+const error = chalk.red;
+
+if (major < 17) {
+  console.log(
+    error(
+      "You are running Node " +
+        currentNodeVersion +
+        ".\n" +
+        "npx react-faststart requires Node 17 or higher. \n" +
+        "Please update your version of Node."
+    )
+  );
+  process.exit(1);
+}
 
 // Validate the project name based on security requirements
 const validateProjectName = (name) => {
+  checkAppName(name);
   const validName = /^[a-z][a-z0-9_]*$/.test(name);
   if (!validName) {
     console.log(
-      "Error: Project name must start with a letter and can only contain lowercase letters, numbers, and underscores."
+      error(
+        "Error: Project name must start with a letter and can only contain lowercase letters, numbers, and underscores."
+      )
     );
     return false;
   }
   return true;
+};
+
+const setUpTailwindCSS = async (projectName, templateChoice) => {
+  process.chdir(projectName);
+
+  // Step 5: Install dependencies and configure the project
+  console.log("Installing Tailwind CSS...");
+  await execa(
+    "npm",
+    ["install", "-D", "tailwindcss", "postcss", "autoprefixer"],
+    { stdio: "inherit" }
+  );
+
+  console.log(chalk.green("Dependencies installed successfully!"));
+
+  // Step 6: Initialize Tailwind CSS configuration
+  console.log("Initializing Tailwind configuration...");
+  await execa("npx", ["tailwindcss", "init", "-p"], { stdio: "inherit" });
+
+  // Step 7: Update `tailwind.config.js` file
+  console.log("Configuring Tailwind...");
+  const tailwindConfigPath = path.resolve("tailwind.config.js");
+  const tailwindConfig = `/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+`;
+  await fs.promises.writeFile(tailwindConfigPath, tailwindConfig);
+
+  // Step 8: Create and update the `src/index.css` file
+  console.log("Setting up Tailwind styles...");
+  const srcDir = path.resolve("src");
+  await fs.promises.mkdir(srcDir, { recursive: true });
+
+  const indexCSSPath = path.resolve(srcDir, "index.css");
+  const tailwindDirectives = `@tailwind base;
+@tailwind components;
+@tailwind utilities;
+`;
+  await fs.promises.writeFile(indexCSSPath, tailwindDirectives);
+
+  // Step 9: Remove `App.css` and create the appropriate `App` file
+  const appCssPath = path.resolve(srcDir, "App.css");
+  if (fs.existsSync(appCssPath)) {
+    console.log("Deleting App.css...");
+    await fs.promises.unlink(appCssPath);
+  }
+
+  // Step 10: Reset `App.jsx` or `App.tsx` based on the template
+  const appFilePath = path.join(
+    srcDir,
+    templateChoice === "react-ts" ? "App.tsx" : "App.jsx"
+  );
+  const appFileContent = `import React from 'react';
+
+  const App${templateChoice === "react-ts" ? ": React.FC" : ""} = () => {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+        <header className="text-center">
+          <h1 className="text-5xl font-extrabold mb-4">Welcome to My Tailwind App</h1>
+          <p className="text-lg font-light">A React project styled with Tailwind CSS</p>
+        </header>
+        <main className="mt-8">
+          <a href="https://www.npmjs.com/package/react-faststart" target="_blank" className="px-6 py-3 bg-white text-blue-600 font-semibold rounded-md shadow-lg hover:bg-blue-100 transition">
+            Get Started
+          </a>
+        </main>
+      </div>
+    );
+  };
+  
+  export default App;
+  `;
+
+  console.log(
+    `Resetting ${templateChoice === "react-ts" ? "App.tsx" : "App.jsx"}...`
+  );
+  await fs.promises.writeFile(appFilePath, appFileContent);
 };
 
 const run = async () => {
@@ -38,7 +146,7 @@ const run = async () => {
     const projectDir = path.resolve(__dirname, projectName);
 
     // Step 2: Check if the directory already exists
-    if (fs.existsSync(projectName)) {
+    if (fs.existsSync(projectDir)) {
       const { overwrite } = await inquirer.prompt([
         {
           type: "confirm",
@@ -49,7 +157,7 @@ const run = async () => {
       ]);
 
       if (!overwrite) {
-        console.log("Operation cancelled.");
+        console.log(error("Operation cancelled."));
         return;
       }
 
@@ -69,7 +177,9 @@ const run = async () => {
     ]);
 
     console.log(
-      `Creating Vite project '${projectName}' with template '${templateChoice}'...`
+      chalk.green(
+        `Creating Vite project '${projectName}' with template '${templateChoice}'...`
+      )
     );
 
     // Step 4: Create a new Vite project
@@ -79,82 +189,26 @@ const run = async () => {
       { stdio: "inherit" }
     );
 
-    console.log("Project created successfully!");
+    console.log(chalk.green("Project created successfully!"));
+    console.log(chalk.blue("-----------------------------------"));
 
-    process.chdir(projectName);
+    // Check default or tailwind css installation
 
-    // Step 5: Install dependencies and configure the project
-    console.log("Installing Tailwind CSS...");
-    await execa(
-      "npm",
-      ["install", "-D", "tailwindcss", "postcss", "autoprefixer"],
-      { stdio: "inherit" }
-    );
+    const { setupChoice } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "setupChoice",
+        message: "Choose your project setup:",
+        choices: ["Default", "Tailwind CSS"],
+      },
+    ]);
 
-    console.log("Dependencies installed successfully!");
-
-    // Step 6: Initialize Tailwind CSS configuration
-    console.log("Initializing Tailwind configuration...");
-    await execa("npx", ["tailwindcss", "init", "-p"], {
-      stdio: "inherit",
-    });
-
-    // Step 7: Update `tailwind.config.js` file
-    console.log("Configuring Tailwind...");
-    const tailwindConfigPath = path.resolve("tailwind.config.js");
-    const tailwindConfig = `/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-`;
-    await fs.promises.writeFile(tailwindConfigPath, tailwindConfig);
-
-    // Step 8: Create and update the `src/index.css` file
-    console.log("Setting up Tailwind styles...");
-    const srcDir = path.resolve("src");
-    await fs.promises.mkdir(srcDir, { recursive: true });
-
-    const indexCSSPath = path.resolve(srcDir, "index.css");
-    const tailwindDirectives = `@tailwind base;
-@tailwind components;
-@tailwind utilities;
-`;
-    await fs.promises.writeFile(indexCSSPath, tailwindDirectives);
-
-    // Step 9: Remove `App.css` and create the appropriate `App` file
-    const appCssPath = path.resolve(srcDir, "App.css");
-    if (fs.existsSync(appCssPath)) {
-      console.log("Deleting App.css...");
-      await fs.promises.unlink(appCssPath);
+    if (setupChoice === "Tailwind CSS") {
+      // Tailwind CSS Setup
+      await setUpTailwindCSS(projectName, templateChoice);
+    } else {
+      await execa("npm", ["install"], { stdio: "inherit" });
     }
-
-    // Step 10: Reset `App.jsx` or `App.tsx` based on the template
-    const appFilePath = path.resolve(
-      srcDir,
-      templateChoice === "react-ts" ? "App.tsx" : "App.jsx"
-    );
-    const appFileContent = `import React from 'react';
-
-const App${templateChoice === "react-ts" ? ": React.FC" : ""} = () => {
-  return (
-    <div>App</div>
-  );
-}
-
-export default App;
-`;
-
-    console.log(
-      `Resetting ${templateChoice === "react-ts" ? "App.tsx" : "App.jsx"}...`
-    );
-    await fs.promises.writeFile(appFilePath, appFileContent);
 
     // Step 11: Final instructions to the user
     console.log(
---
+++ src/helper/reactApp.js
@@ -0,0 +1,73 @@
+import path from "path";
+import chalk from "chalk";
+import validateProjectName from "validate-npm-package-name";
+
+function getTemplateInstallPackage(template, originalDirectory) {
+  let templateToInstall = "cra-template";
+  if (template) {
+    if (template.match(/^file:/)) {
+      templateToInstall = `file:${path.resolve(
+        originalDirectory,
+        template.match(/^file:(.*)?$/)[1]
+      )}`;
+    } else if (
+      template.includes("://") ||
+      template.match(/^.+\.(tgz|tar\.gz)$/)
+    ) {
+      // for tar.gz or alternative paths
+      templateToInstall = template;
+    } else {
+      // Add prefix 'cra-template-' to non-prefixed templates, leaving any
+      // @scope/ and @version intact.
+      const packageMatch = template.match(/^(@[^/]+\/)?([^@]+)?(@.+)?$/);
+      const scope = packageMatch[1] || "";
+      const templateName = packageMatch[2] || "";
+      const version = packageMatch[3] || "";
+
+      if (
+        templateName === templateToInstall ||
+        templateName.startsWith(`${templateToInstall}-`)
+      ) {
+        // Covers:
+        // - cra-template
+        // - @SCOPE/cra-template
+        // - cra-template-NAME
+        // - @SCOPE/cra-template-NAME
+        templateToInstall = `${scope}${templateName}${version}`;
+      } else if (version && !scope && !templateName) {
+        // Covers using @SCOPE only
+        templateToInstall = `${version}/${templateToInstall}`;
+      } else {
+        // Covers templates without the `cra-template` prefix:
+        // - NAME
+        // - @SCOPE/NAME
+        templateToInstall = `${scope}${templateToInstall}-${templateName}${version}`;
+      }
+    }
+  }
+
+  return Promise.resolve(templateToInstall);
+}
+
+const checkAppName = (appName) => {
+  const validationResult = validateProjectName(appName);
+  if (!validationResult.validForNewPackages) {
+    console.error(
+      chalk.red(
+        `Cannot create a project named ${chalk.green(
+          `"${appName}"`
+        )} because of npm naming restrictions:\n`
+      )
+    );
+    [
+      ...(validationResult.errors || []),
+      ...(validationResult.warnings || []),
+    ].forEach((error) => {
+      console.error(chalk.red(`  * ${error}`));
+    });
+    console.error(chalk.red("\nPlease choose a different project name."));
+    process.exit(1);
+  }
+};
+
+export { getTemplateInstallPackage, checkAppName };
